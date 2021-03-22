5a93ec93eb551e5b7fca389d3c58f72cc6715939
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-03-22 13:52:37 -0400

    created file, with game plan list of what i want to do.

---
+++ main.py
@@ -0,0 +1,28 @@
+if __name__ == '__main__':
+
+
+
+# introduction
+# ask user if they want to play another human or cpu
+
+# display rules
+# best 2 out of 3 wins
+# Rock crushes Scissors
+# Scissors cuts Paper
+# Paper covers Rock
+# Rock crushes Lizard
+# Lizard poisons Spock
+# Spock smashes Scissors
+# Scissors decapitates Lizard
+# Lizard eats Paper
+# Paper disproves Spock
+# Spock vaporizes Rock
+
+
+# game starts with 1,2,3...shoot
+# after winner is chosen ask if they would like to remain champion
+# ask loser if they want redemption
+
+# plays again
+
+
