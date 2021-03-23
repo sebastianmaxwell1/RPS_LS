9215277688fab45c4901761e618c15326501c955
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-03-23 15:04:09 -0400

    created values and comparisons

---
+++ player.py
@@ -0,0 +1,10 @@
+from game import Game
+
+
+class Player:
+
+    def __init__(self, user_player, computer_player):
+        self.user_gestures = user_player
+        self.computer_gestures = computer_player
+
+    def
